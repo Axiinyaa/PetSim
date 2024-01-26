33f85c4c92cff250226be78607563b32bc7571bd
Axiinyaa <derfii2004@gmail.com>
2024-01-26 20:50:27 +0000

    Initial commit

---
+++ prefabs/draggable_object.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var dragging: bool
+var hovering: bool
+
+func _on_area_2d_mouse_entered():
+	hovering = true
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	
+func _on_area_2d_mouse_exited():
+	hovering = false
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+	
+func dragging_logic():
+	if Input.is_action_pressed("interaction"):
+		if not dragging:
+			if not hovering:
+				return
+			
+			dragging = true
+			return
+	else:
+		dragging = false
+		return
+
+func _process(delta):
+	dragging_logic()
+	
+	if dragging:
+		global_position = lerp(global_position, get_global_mouse_position(), 10 * delta)
